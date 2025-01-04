2b9b06e6028b4fe14a519ffafda04aea3870fd9c
Marcel Wiechmann <7428876+MWiechmann@users.noreply.github.com>
2025-01-04 16:56:53 +0100

    Time: 5618 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- sql50/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ sql50/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,4 +1,5 @@
 -- PostgreSQL
+/*
 SELECT
     contest_id,
     ROUND(
@@ -8,6 +9,38 @@ SELECT
     )*100 AS percentage
 FROM
     register
+GROUP BY contest_id
+ORDER BY
+    percentage DESC,
+    contest_id ASC
+;
+*/
+
+-- Trying out alternative solution:
+WITH 
+    unique_contests AS(
+        SELECT DISTINCT contest_id
+        FROM register
+    ),
+    individual_user_atendance AS(
+        SELECT
+            uc.contest_id,
+            us.user_id,
+            EXISTS(
+                SELECT 1
+                FROM register AS re
+                WHERE
+                    us.user_id=re.user_id
+                    AND uc.contest_id = re.contest_id
+                ) AS user_attended
+        FROM
+            unique_contests AS uc
+            CROSS JOIN users AS us
+    )
+SELECT
+    contest_id,
+    ROUND(AVG(user_attended::int)*100,2) AS percentage
+FROM individual_user_atendance
 GROUP BY contest_id
 ORDER BY
     percentage DESC,
