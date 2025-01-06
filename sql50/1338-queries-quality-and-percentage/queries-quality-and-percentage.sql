9c7b5968221444dcb7ed3fe5b3a32966ec9e25ca
Marcel Wiechmann <7428876+MWiechmann@users.noreply.github.com>
2025-01-06 11:08:16 +0100

    Time: 769 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

---
+++ sql50/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,26 @@
+-- PostgreSQL
+
+WITH cnt_poor_qual AS(
+    SELECT
+        query_name,
+        COUNT(query_name) AS count_poor_query_qual
+    FROM queries
+    WHERE (rating) < 3 GROUP BY query_name
+)
+SELECT
+    q.query_name,
+    ROUND(
+        AVG(q.rating::NUMERIC/q.position::NUMERIC)
+        ,2) AS quality,
+    ROUND(
+        COALESCE(
+            (cpq.count_poor_query_qual::NUMERIC/COUNT(q.query_name)::NUMERIC)*100
+            ,0)
+        ,2) AS poor_query_percentage 
+FROM
+    queries AS q
+    LEFT JOIN cnt_poor_qual AS cpq
+        ON q.query_name = cpq.query_name
+GROUP BY
+    q.query_name,
+    cpq.count_poor_query_qual;
