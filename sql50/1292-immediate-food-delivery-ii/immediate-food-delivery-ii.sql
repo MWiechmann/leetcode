f8c745177420bcdec20cb8629a8d4d32741c4f0f
Marcel Wiechmann <7428876+MWiechmann@users.noreply.github.com>
2025-01-07 12:05:33 +0100

    Time: 865 ms (14.33%) | Memory: 0B (100.00%) - LeetSync

--- sql50/1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ sql50/1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,4 +1,5 @@
 -- PostgreSQL
+/*
 WITH first_order AS (
     SELECT
         customer_id,
@@ -20,3 +21,22 @@ FROM
         ON d.customer_id = fo.customer_id
         AND d.order_date = fo.order_date
 ;
+*/
+WITH ranked_orders AS (
+    SELECT
+        customer_id,
+        RANK() OVER (PARTITION BY customer_id ORDER BY order_date ASC) as order_rank,
+        order_date,
+        customer_pref_delivery_date
+    FROM delivery
+)
+SELECT
+    ROUND(
+        AVG(CASE
+        WHEN order_date = customer_pref_delivery_date THEN 1
+        ELSE 0
+        END)*100
+    ,2) AS immediate_percentage 
+FROM ranked_orders
+WHERE
+    order_rank = 1;
