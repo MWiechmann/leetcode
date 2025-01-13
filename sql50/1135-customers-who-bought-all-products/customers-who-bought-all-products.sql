539cd2c773b11a0a85749fd8dfd35c3130e40181
Marcel Wiechmann <7428876+MWiechmann@users.noreply.github.com>
2025-01-13 13:56:35 +0100

    Time: 964 ms (28.10%) | Memory: 0B (100.00%) - LeetSync

--- sql50/1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ sql50/1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -4,3 +4,19 @@ SELECT
 FROM customer
 GROUP BY customer_id
 HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM product)
+
+/*
+-- Alternative solution using correlated subquery
+-- Computationally more involved ofc, but wante to practice those
+SELECT customer_id
+FROM Customer c
+WHERE NOT EXISTS (
+    SELECT 1 
+    FROM Product p
+    WHERE p.product_key NOT IN (
+        SELECT product_key
+        FROM Customer c2
+        WHERE c2.customer_id = c.customer_id
+    )
+)
+*/
