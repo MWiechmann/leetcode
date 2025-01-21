c2dfa4b9120b83aeee069948f8a1222c407ae90e
Marcel Wiechmann <7428876+MWiechmann@users.noreply.github.com>
2025-01-21 17:23:58 +0100

    Time: 916 ms (15.18%) | Memory: 0B (100.00%) - LeetSync

--- sql50/1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ sql50/1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,4 +1,5 @@
 -- PostgreSQL
+-- CTE that just gives us starting price and all product_ids
 WITH start_price AS
 (
     SELECT DISTINCT
@@ -6,6 +7,9 @@ WITH start_price AS
         10 AS start_price
     FROM products
 )
+-- CTE that gives us the prices changes in the relevant date range
+-- also add a column with the date of the latest price change for that product_id
+-- (windows function occur after WHERE so we can not filter for the latest price change yet)
 , latest_price_change AS
 (
     SELECT
@@ -17,6 +21,12 @@ WITH start_price AS
     WHERE
         change_date <= '2019-08-16'
 )
+-- Bring it together:
+-- LEFT JOIN start prices and latest_price_change
+-- Keep only values that either have no price change at all in the relevant date range
+-- (and therefore no entries in latest_price_change)
+-- or keep only the row for the latest price change
+-- We can build the price from start price and latest price change depending on where the product_id has entries
 SELECT
     sp.product_id,
     CASE
