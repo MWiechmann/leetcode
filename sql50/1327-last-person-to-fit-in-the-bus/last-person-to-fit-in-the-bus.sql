99e6b073a90a1035205a83a2517ede11993b241c
Marcel Wiechmann <7428876+MWiechmann@users.noreply.github.com>
2025-01-29 12:35:30 +0100

    Time: 905 ms (39.31%) | Memory: 0B (100.00%) - LeetSync

---
+++ sql50/1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,17 @@
+-- PostgreSQL
+WITH running_total_weight AS(
+    SELECT
+        person_name,
+        turn,
+        SUM(weight) OVER (
+            ORDER BY turn ASC
+            ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+            ) AS rt_weight
+    FROM queue
+    ORDER BY turn ASC
+)
+SELECT person_name
+FROM running_total_weight
+WHERE rt_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
