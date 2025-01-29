d2d59652085676a6b57c97078884ce4a9c60decc
Marcel Wiechmann <7428876+MWiechmann@users.noreply.github.com>
2025-01-29 13:36:54 +0100

    Time: 1043 ms (25.94%) | Memory: 0B (100.00%) - LeetSync

--- sql50/1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ sql50/1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,4 +1,5 @@
 -- PostgreSQL
+/*
 WITH running_total_weight AS(
     SELECT
         person_name,
@@ -13,5 +14,15 @@ WITH running_total_weight AS(
 SELECT person_name
 FROM running_total_weight
 WHERE rt_weight <= 1000
+ORDER BY turn DESC
+LIMIT 1;
+*/
+
+-- Alternative solution using correlated subqueries
+SELECT
+    person_name
+FROM queue as q
+WHERE (SELECT SUM(weight) FROM queue AS q2
+    WHERE q2.turn <= q.turn) <= 1000
 ORDER BY turn DESC
 LIMIT 1;
