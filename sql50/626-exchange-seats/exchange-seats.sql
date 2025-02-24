5ae708453a920a52171b5c32f981f98afc180764
Marcel Wiechmann <7428876+MWiechmann@users.noreply.github.com>
2025-02-24 16:22:39 +0100

    Time: 422 ms (26.26%) | Memory: 0B (100.00%) - LeetSync

--- sql50/626-exchange-seats/exchange-seats.sql
+++ sql50/626-exchange-seats/exchange-seats.sql
@@ -1,16 +1,10 @@
 -- PostgreSQL
 SELECT
-    id-1 AS id,
-    student
-FROM seat
-WHERE mod(id,2) = 0
-UNION ALL
-SELECT
-    CASE 
-        WHEN id = (SELECT MAX(id) FROM seat) THEN id
+    CASE
+        WHEN mod(id,2) = 0 THEN id-1
+        WHEN (mod(id,2) <> 0) AND id = (SELECT MAX(id) FROM seat) THEN id
         ELSE id+1
     END  AS id,
     student
 FROM seat
-WHERE mod(id,2) <> 0
 ORDER BY id ASC
