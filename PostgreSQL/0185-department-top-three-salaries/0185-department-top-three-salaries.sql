4fb45948da939093c3c676e76cefbfff0cd71183
Marcel Wiechmann <7428876+MWiechmann@users.noreply.github.com>
2026-05-05 11:35:14 +0200

    Time: 288 ms (26.92%), Space: 0B (100%) - LeetHub

---
+++ PostgreSQL/0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,22 @@
+-- Write your PostgreSQL query statement below
+WITH salaries_drank AS (
+    SELECT
+        d.name AS department,
+        e.name AS employee,
+        salary,
+        DENSE_RANK() OVER (
+                PARTITION BY e.departmentID
+                ORDER BY e.salary DESC
+            ) AS rank
+    FROM employee AS e
+    INNER JOIN department as d
+        ON e.departmentId = d.id
+    ORDER BY departmentid, rank
+)
+SELECT
+    department AS "Department",
+    employee AS "Employee",
+    salary AS "Salary"
+FROM salaries_drank
+WHERE rank <= 3
+ORDER BY department, rank
